3b5f976e502ebc56bd1413d52b14a3aa2cc65ab0
axleiro <83293196+axleiro@users.noreply.github.com>
2021-11-27 18:01:40 +0530

    commenting the command "os.system('gcloud init')"

--- script.py
+++ script.py
@@ -16,7 +16,7 @@ os.system('sudo apt-get install apt-transport-https ca-certificates gnupg')
 os.system('echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list')
 os.system('curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -')
 os.system('sudo apt-get update && sudo apt-get install google-cloud-sdk')
-os.system('gcloud init')
+# os.system('gcloud init')
 os.system('')
 
 # To create a configuration to use while working on the testnet project
