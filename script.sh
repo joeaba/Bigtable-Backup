a97869c761ffb8bc958e39412ea311a7e9ccdd07
axleiro <83293196+axleiro@users.noreply.github.com>
2021-12-02 21:18:16 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -x
-trap read debug
+# trap read debug
 sudo apt-get install apt-transport-https ca-certificates gnupg
 echo "sudo apt-get install apt-transport-https ca-certificates gnupg"
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
