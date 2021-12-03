07e902d637b1310e7f502da53605bcf7267ee71b
axleiro <83293196+axleiro@users.noreply.github.com>
2021-12-03 17:41:16 +0530

    adding the command for description
    {
    #creating the config for the GCP
    #reading the content of the .json file
    #exporting and authenticating the GCP service account
    }

--- env_test.sh
+++ env_test.sh
@@ -2,5 +2,43 @@
 touch testnet_bt_readwrite.json
 cat <<< "$TESTNET_SERVICE_ACCOUNT_JSON" > testnet_bt_readwrite.json
 cat testnet_bt_readwrite.json
+pwd
 ls
 
+sudo apt-get install apt-transport-https ca-certificates gnupg
+echo "sudo apt-get install apt-transport-https ca-certificates gnupg"
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+echo "curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -"
+status = $(sudo apt-get update && sudo apt-get install google-cloud-sdk )
+#   if [[ -n $status ]]; then
+#     echo "Installed the google-cloud-sdk"
+#     exit 0
+#   else
+#     echo "google-cloud-sdk is previously installed on this system"
+#   fi
+
+#creating the config for the GCP
+gcloud config set account bigtable-backup-read-write@tour-de-sol.iam.gserviceaccount.com
+echo "gcloud config set account bigtable-backup-read-write@tour-de-sol.iam.gserviceaccount.com"
+gcloud config set project tour-de-sol
+gcloud config configurations list
+
+declare file_path="$0"
+echo "file path is: "$file_path""
+# cd file_path
+
+#declaring a variable to store the value of the vaiable from the .yml file
+# echo "$(snp "$TESTNET_SERVICE_ACCOUNT_JSON")"
+declare snp="$TESTNET_SERVICE_ACCOUNT_JSON"
+echo "snp is: "$snp""
+pwd
+
+#reading the content of the .json file
+cat testnet_bt_readwrite.json
+pwd
+head testnet_bt_readwrite.json
+
+#exporting and authenticating the GCP service account
+export GOOGLE_APPLICATION_CREDENTIALS=./testnet_bt_readwrite.json
+gcloud auth activate-service-account bigtable-backup-read-write@tour-de-sol.iam.gserviceaccount.com --key-file=./testnet_bt_readwrite.json
