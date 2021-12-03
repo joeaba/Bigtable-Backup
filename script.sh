748b6b840fc259aeb0d40b1891ae02ebff58dee3
axleiro <83293196+axleiro@users.noreply.github.com>
2021-12-03 16:08:47 +0530

    commenting and uncommenting the necessary commands
    {
    # echo "$(file_path "$0")"
    declare file_path="$0"
    
    # echo "$(snp "$TESTNET_SERVICE_ACCOUNT_JSON")"
    declare snp="$TESTNET_SERVICE_ACCOUNT_JSON"
    }

--- script.sh
+++ script.sh
@@ -19,14 +19,14 @@ echo "gcloud config set account bigtable-backup-read-write@tour-de-sol.iam.gserv
 gcloud config set project tour-de-sol
 gcloud config configurations list
 # cd /home/
-echo "$(file_path "$0")"
-# declare file_path="$0"
+# echo "$(file_path "$0")"
+declare file_path="$0"
 echo "file path is: "$file_path""
 cd "$(file_path)"
 
 #declaring a variable to store the value of the vaiable from the .yml file
-echo "$(snp "$TESTNET_SERVICE_ACCOUNT_JSON")"
-# declare snp="$TESTNET_SERVICE_ACCOUNT_JSON"
+# echo "$(snp "$TESTNET_SERVICE_ACCOUNT_JSON")"
+declare snp="$TESTNET_SERVICE_ACCOUNT_JSON"
 echo "snp is "$snp""
 
 #printing credentials inside the file
