7ce8b9a0423283b2280b5b4b6e97f03d40afc5b5
axleiro <83293196+axleiro@users.noreply.github.com>
2021-12-04 15:31:40 +0530

    commenting the " sudo apt-get install apt-transport-https ca-certificates gnupg" to skip it

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 set -x
 # trap read debug
 
-sudo apt-get install apt-transport-https ca-certificates gnupg
+# sudo apt-get install apt-transport-https ca-certificates gnupg
 echo "sudo apt-get install apt-transport-https ca-certificates gnupg"
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
