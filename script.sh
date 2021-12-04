a97234eb9869c40a1f6fbb1ef654f8b0d80adc9f
axleiro <83293196+axleiro@users.noreply.github.com>
2021-12-04 16:35:43 +0530

    handling the error created due to the incorrect "/" inplace of "\"

--- script.sh
+++ script.sh
@@ -92,13 +92,13 @@ ls
 
 #adding the path to the "GOOGLE_APPLICATION_CREDENTIALS" and also authenticating the service account
 export GOOGLE_APPLICATION_CREDENTIALS=/home/joe/testnet_bt_readwrite.json
-gcloud auth activate-service-account bigtable-backup-read-write@tour-de-sol.iam.gserviceaccount.com /
+gcloud auth activate-service-account bigtable-backup-read-write@tour-de-sol.iam.gserviceaccount.com \
 --key-file=/home/joe/testnet_bt_readwrite.json
 
 #command to create the bigtable backup
-gcloud bigtable backups create solana-bigtable-backup --instance=solana-ledger /
---cluster=solana-ledger-c1 /
---table=test2 /
+gcloud bigtable backups create solana-bigtable-backup --instance=solana-ledger \
+--cluster=solana-ledger-c1 \
+--table=test2 \
 --retention-period=3d
 
 
