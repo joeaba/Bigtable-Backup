b5f43943dc74f80736d2a0e1d04fa6906f13736c
joeaba <77398477+joeaba@users.noreply.github.com>
2021-12-21 12:16:57 +0530

    tracing the file content

--- script.sh
+++ script.sh
@@ -5,6 +5,7 @@ set -x
 
 # sudo apt-get install apt-transport-https ca-certificates gnupg
 echo "sudo apt-get install apt-transport-https ca-certificates gnupg"
+
 # echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 echo "curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -"
